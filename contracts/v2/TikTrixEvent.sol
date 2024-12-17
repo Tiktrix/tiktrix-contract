cbdbeefc959db3a92bfdd38f07f79844c00c29f8
realwater <pie1104@nate.com>
2024-12-17 17:17:58 +0900

    불필요한 소스 제거 및 컨트랙트 변경 반영

--- contracts/v2/TikTrixEvent.sol
+++ contracts/v2/TikTrixEvent.sol
@@ -1,12 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.8.0/contracts/token/ERC20/IERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.8.0/contracts/access/AccessControl.sol";
 
-contract tTikTrixAction is AccessControl {
+contract tTikTrixEvent is AccessControl {
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
 
+    struct MemberInfo {
+        uint256 memberSeq;
+    }
+
+    mapping(uint256 => MemberInfo) public memberInfos;
+    uint256[] public memberIds;
+
+    event MemberRegistered(uint256 memberSeq);
+    event RankScoreUpdateNoraml(uint256 indexed yyyymmdd, uint256 indexed gameSeq, uint256 indexed memberSeq, uint256 newScore);
+    
     event GamePlayed(uint256 indexed gameSeq, uint256 indexed memberSeq);
     event GameEnded(uint256 indexed gameSeq, uint256 indexed memberSeq);
     event GameLiked(uint256 indexed gameSeq, uint256 indexed memberSeq);
@@ -22,6 +31,27 @@ contract tTikTrixAction is AccessControl {
 
     function revokeAdminRole(address account) public onlyRole(DEFAULT_ADMIN_ROLE) {
         revokeRole(ADMIN_ROLE, account);
+    }
+
+    function memberRegister(uint256 memberSeq) external onlyRole(ADMIN_ROLE) {
+        require(memberInfos[memberSeq].memberSeq == 0, "Member already registered");
+
+        memberInfos[memberSeq] = MemberInfo({
+            memberSeq: memberSeq
+        });
+        memberIds.push(memberSeq);
+
+        emit MemberRegistered(memberSeq);
+    }
+
+    function rankScoreUpdateNormal(
+        uint256 yyyymmdd,
+        uint256 gameSeq,
+        uint256 memberSeq,
+        uint256 newScore
+    ) external onlyRole(ADMIN_ROLE) {
+       
+        emit RankScoreUpdateNoraml(yyyymmdd, gameSeq, memberSeq, newScore);
     }
 
     function gamePlay(uint256 gameSeq, uint256 memberSeq) external {
