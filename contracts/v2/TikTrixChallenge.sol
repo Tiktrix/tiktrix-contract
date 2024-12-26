fa5cac6f1aac79c719aba7bcae5ae47067988443
realwater <pie1104@nate.com>
2024-12-26 13:33:55 +0900

    TikTrixChallenge 컨트랙트 추가

---
+++ contracts/v2/TikTrixChallenge.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.8.0/contracts/access/AccessControl.sol";
+import "./TikTrixEscrow.sol";
+
+contract TikTrixChallenge is AccessControl {
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+
+    TikTrixEscrow public escrow;    
+    mapping(uint256 => mapping(uint256 => mapping(uint256 => uint256))) public highScores;
+    
+    event ScoreUpdated(
+        uint256 baseDate, 
+        uint256 gameSeq, 
+        uint256 memberSeq, 
+        uint256 newScore,
+        uint256 previousScore
+    );
+    
+    constructor(address _escrowAddress) {
+        escrow = TikTrixEscrow(_escrowAddress);
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(ADMIN_ROLE, msg.sender);
+    }
+
+    function grantAdminRole(address account) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        grantRole(ADMIN_ROLE, account);
+    }
+
+    function revokeAdminRole(address account) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        revokeRole(ADMIN_ROLE, account);
+    }
+    
+    function rankScoreUpdateChallenge(
+        uint256 baseDate,
+        uint256 gameSeq,
+        uint256 memberSeq,
+        uint256 newScore
+    ) external onlyRole(ADMIN_ROLE) {
+        (uint256 depositAmount, bool isReturned) = escrow.getDepositFee(baseDate, gameSeq, memberSeq);
+        require(depositAmount > 0, "No deposit found for this challenge");
+        require(!isReturned, "Deposit has already been returned");        
+
+        uint256 currentHighScore = highScores[baseDate][gameSeq][memberSeq];
+        
+        if(newScore > currentHighScore) {
+            highScores[baseDate][gameSeq][memberSeq] = newScore;
+        }
+        
+        emit ScoreUpdated(baseDate, gameSeq, memberSeq, newScore, currentHighScore);
+    }
+    
+    function getHighScore(uint256 baseDate, uint256 gameSeq, uint256 memberSeq) 
+        external 
+        view 
+        returns (uint256)
+    {
+        return highScores[baseDate][gameSeq][memberSeq];
+    }
+}
