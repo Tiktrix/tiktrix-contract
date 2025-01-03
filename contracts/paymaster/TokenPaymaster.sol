3d09676546fe6b89c3302754aac664def0845e59
realwater <pie1104@nate.com>
2025-01-03 17:10:38 +0900

    토큰 페이마스터 컨트랙트 추가 배포

---
+++ contracts/paymaster/TokenPaymaster.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import "@account-abstraction/contracts/interfaces/IPaymaster.sol";
+import "@account-abstraction/contracts/interfaces/IEntryPoint.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TokenPaymaster is IPaymaster, Ownable {
+    IEntryPoint public immutable entryPoint;
+    
+    mapping(address => bool) public supportedTokens;
+    mapping(address => uint256) public userGasLimits;
+    
+    constructor(IEntryPoint _entryPoint) Ownable(msg.sender) {
+        entryPoint = _entryPoint;
+    }
+
+    function validatePaymasterUserOp(
+        PackedUserOperation calldata userOp,
+        bytes32 userOpHash,
+        uint256 maxCost
+    ) external virtual override returns (bytes memory context, uint256 validationData) {
+        require(msg.sender == address(entryPoint), "Sender not EntryPoint");
+        require(userGasLimits[userOp.sender] >= maxCost, "Gas limit exceeded");
+        
+        return (abi.encode(userOp.sender), 0);
+    }
+
+    function postOp(
+        PostOpMode mode,
+        bytes calldata context,
+        uint256 actualGasCost,
+        uint256 actualUserOpFeePerGas
+    ) external override {
+        require(msg.sender == address(entryPoint), "Sender not EntryPoint");
+        
+        address sender = abi.decode(context, (address));
+        userGasLimits[sender] -= actualGasCost;
+    }
+
+    function addSupportedToken(address token) external onlyOwner {
+        supportedTokens[token] = true;
+    }
+
+    function setUserGasLimit(address user, uint256 limit) external onlyOwner {
+        userGasLimits[user] = limit;
+    }
+
+    function deposit() public payable {
+        entryPoint.depositTo{value: msg.value}(address(this));
+    }
+
+    function withdrawTo(address payable to, uint256 amount) external onlyOwner {
+        entryPoint.withdrawTo(to, amount);
+    }
+
+    function getDeposit() public view returns (uint256) {
+        return entryPoint.balanceOf(address(this));
+    }
+}
