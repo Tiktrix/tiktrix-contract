4b9b6df93a7eeff666c8ca58123c32ad3c3bafd6
realwater <pie1104@nate.com>
2025-01-03 17:36:46 +0900

    페이마스터 권한 부여 및 처리 개선

--- contracts/paymaster/TokenPaymaster.sol
+++ contracts/paymaster/TokenPaymaster.sol
@@ -4,15 +4,27 @@ pragma solidity ^0.8.26;
 import "@account-abstraction/contracts/interfaces/IPaymaster.sol";
 import "@account-abstraction/contracts/interfaces/IEntryPoint.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract TokenPaymaster is IPaymaster, Ownable {
+contract TokenPaymaster is IPaymaster, Ownable, AccessControl {
     IEntryPoint public immutable entryPoint;
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
     
     mapping(address => bool) public supportedTokens;
     mapping(address => uint256) public userGasLimits;
     
     constructor(IEntryPoint _entryPoint) Ownable(msg.sender) {
         entryPoint = _entryPoint;
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(ADMIN_ROLE, msg.sender);
+    }
+
+    function grantAdminRole(address account) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _grantRole(ADMIN_ROLE, account);
+    }
+
+    function revokeAdminRole(address account) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _revokeRole(ADMIN_ROLE, account);
     }
 
     function validatePaymasterUserOp(
@@ -38,11 +50,11 @@ contract TokenPaymaster is IPaymaster, Ownable {
         userGasLimits[sender] -= actualGasCost;
     }
 
-    function addSupportedToken(address token) external onlyOwner {
+    function addSupportedToken(address token) external onlyRole(ADMIN_ROLE) {
         supportedTokens[token] = true;
     }
 
-    function setUserGasLimit(address user, uint256 limit) external onlyOwner {
+    function setUserGasLimit(address user, uint256 limit) external onlyRole(ADMIN_ROLE) {
         userGasLimits[user] = limit;
     }
 
@@ -56,5 +68,9 @@ contract TokenPaymaster is IPaymaster, Ownable {
 
     function getDeposit() public view returns (uint256) {
         return entryPoint.balanceOf(address(this));
+    }
+
+    function existsUserGasLimit(address user) external view returns (bool) {
+        return userGasLimits[user] > 0;
     }
 }
