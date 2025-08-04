0593374b8fa0dc895f4598dd349d6aabd1f93b4a
realwater <pie1104@nate.com>
2025-08-04 16:50:29 +0900

    컨트랙트 불필요한 정보 삭제

--- contract/vesting/finance/v1/VestingWalletAdvisors.sol
+++ contract/vesting/finance/v1/VestingWalletAdvisors.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {IERC20} from "../token/ERC20/IERC20.sol";
-import {SafeERC20} from "../token/ERC20/utils/SafeERC20.sol";
-import {Context} from "../utils/Context.sol";
-import {Ownable} from "../access/Ownable.sol";
+import {IERC20} from "../../token/ERC20/IERC20.sol";
+import {SafeERC20} from "../../token/ERC20/utils/SafeERC20.sol";
+import {Context} from "../../utils/Context.sol";
+import {Ownable} from "../../access/Ownable.sol";
 
 import "@thirdweb-dev/contracts/extension/PermissionsEnumerable.sol";
 import "@thirdweb-dev/contracts/extension/ContractMetadata.sol";
